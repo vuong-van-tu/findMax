c5c7538fc648258b4dfd2904ab651219e1bbe329
vuongvantu <vuongvantu2001x@gmail.com>
2021-05-06 21:50:30 +0700

    'findMax'

---
+++ src/com/company/Main.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        int size;
+        int array[];
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.print("Enter a size :");
+            size = sc.nextInt();
+            if (size > 20) {
+                System.out.println("Size should not exceed 20");
+            }
+        } while (size > 20);
+        array = new int[size];
+        int i = 0;
+        while (i<array.length){
+            System.out.print("Enter element " + (i + 1) + " : ");
+            array[i]= sc.nextInt();
+            i++;
+        }
+        System.out.print("Propety list : ");
+        for (int j = 0; j < array.length; j++) {
+            System.out.print(array[j] +"  " );
+        }
+        System.out.println("\n");
+        int max = array[0];
+        int index = 1;
+        for (int j = 0; j < array.length; j++) {
+            if (array[j]>max){
+                max = array[j];
+                index = j + 1;
+            }
+        }
+        System.out.println("GTLN của mảng là : "+max+", vị trí : "+index);
+    }
+}
